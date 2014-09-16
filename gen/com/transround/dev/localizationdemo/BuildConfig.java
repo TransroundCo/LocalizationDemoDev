58473e2c37b8b84ccc90ab4e28b11830065d882b
dongo0223 <peter.donigold@gmail.com>
2014-09-16 13:41:31 +0200

    refactored project layout

---
+++ gen/com/transround/dev/localizationdemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.transround.dev.localizationdemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
